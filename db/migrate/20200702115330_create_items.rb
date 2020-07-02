64a765913b4c124bdad109a154ea263d0bf4c41c
faspro2020 <66356762+faspro2020@users.noreply.github.com>
2020-07-03 00:22:21 +0900

    add items table Options

--- db/migrate/20200702115330_create_items.rb
+++ db/migrate/20200702115330_create_items.rb
@@ -1,17 +1,17 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string :name
-      t.text :introduction
-      t.string :condition
-      t.integer :price
-      t.integer :category_id
-      t.reference :shipping_charges_id
-      t.reference :shipping_from_id
-      t.reference :shipping_days_id
-      t.reference :item_image_id
-      t.reference :seller_id
-      t.reference :buyer_id
+      t.string :name, null: false
+      t.text :introduction, null: false
+      t.string :condition, null: false
+      t.integer :price, null: false
+      t.integer :category_id, null: false, unque: false
+      t.reference :shipping_charges_id, null: false, unque: false
+      t.reference :shipping_from_id, null: false, unque: false
+      t.reference :shipping_days_id, null: false, unque: false
+      t.reference :item_image_id, null: false, unque: false
+      t.reference :seller_id, null: false, unque: false
+      t.reference :buyer_id, null: false, unque: false
       t.timestamps
     end
   end
