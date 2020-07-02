74974514d6b079b2027ae38bd2f496c475e43199
kazuki1722 <66103132+kazuki1722@users.noreply.github.com>
2020-07-02 17:53:35 +0900

    漢字・ひらがなバリデーション修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
   validates :address_family_name, :address_first_name, :address_family_name_kana, :address_first_name_kana, :zipcode, :prefecture, :city, :house_number,presence: true 
-  validates :address_family_name, :address_first_name, :address_family_name_kana, :address_first_name_kana,format: { with: /\A[一-龥ぁ-ん]\z/ }
-  # 漢字ひらがな全角
+  validates :address_family_name, :address_first_name, format: { with: /\A[一-龥]+\z/ }
+  # 漢字
+  validates :address_family_name_kana, :address_first_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
+  # ひらがな
   validates :zipcode, format: { with: /\A\d{7}\z/ }
   # ハイフンなし7桁
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday,presence: true
-  validates :family_name, :first_name, :family_name_kana, :first_name_kana,format: { with: /\A[一-龥ぁ-ん]\z/ }
-  # 漢字ひらがな全角
+  validates :family_name, :first_name, format: { with: /\A[一-龥]+\z/ }
+  # 漢字
+  validates :family_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
+  # ひらがな
   has_one :address
 end
