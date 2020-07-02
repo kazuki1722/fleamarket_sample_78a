1377d6770ac8b4453b20013cb4489a2b5f641687
faspro2020 <66356762+faspro2020@users.noreply.github.com>
2020-07-02 21:19:51 +0900

    create_column type

--- db/migrate/20200702115330_create_items.rb
+++ db/migrate/20200702115330_create_items.rb
@@ -1,7 +1,17 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name
+      t.text :introduction
+      t.string :condition
+      t.integer :price
+      t.integer :category_id
+      t.reference :shipping_charges_id
+      t.reference :shipping_from_id
+      t.reference :shipping_days_id
+      t.reference :item_image_id
+      t.reference :seller_id
+      t.reference :buyer_id
       t.timestamps
     end
   end
