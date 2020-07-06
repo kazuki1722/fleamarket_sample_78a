e7e37751e45c43fb2fb532cc3fd379cd6eb171a5
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-07-06 22:27:28 +0900

    メソッド定義
    newメソッドとcreateメソッドを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,16 @@ class ItemsController < ApplicationController
   end
   
   def new
+    @item = Item.new
+    @item.item_images.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: "出品しました"
+    else 
+      redirect_to new_item_path, alert: "出品できません。入力必須項目を確認してください"
   end
 
   def edit
@@ -16,5 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :condition, :price, :shipping_charges, :shipping_from, :shipping_days, :user_id, item_images_attributes: [:image])
   end
 end
