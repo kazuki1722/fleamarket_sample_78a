e0cf0f95d5d6082c93bace82d4d9b2b805c29803
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-07-06 12:13:14 +0900

    指摘箇所修正②
    ・コントローラ修正
    ・shipping系カラム追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Product.includes(:images).order('created_at DESC')
+    @items = Items.includes(:item_images).order('created_at DESC')
   end
 
   def new
--- db/migrate/20200702115330_create_items.rb
+++ db/migrate/20200702115330_create_items.rb
@@ -5,10 +5,10 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.text :introduction, null: false
       t.string :condition, null: false
       t.integer :price, null: false
-      t.references :category_id
-      t.references :item_image
-      t.references :seller
-      t.references :buyer
+      t.string :shipping_charges, null: false
+      t.string :shipping_from, null: false
+      t.string :shipping_days, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
