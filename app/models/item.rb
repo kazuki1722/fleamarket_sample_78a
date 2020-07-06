30322b2b33f155d5b803f05d83900c90765c0e02
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-07-06 12:28:01 +0900

    item model修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,4 @@
 class Item < ApplicationRecord
-  has_many :item_imgs, dependent: :destroy
-  berongs_to :categorie
-  berongs_to :shipping_charge
-  berongs_to :shipping_from
-  berongs_to :shipping_charge
-  belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  has_many :item_images, dependent: :destroy
+  belongs_to :user
 end
