3796b89f589c3034e0ff672c38a52bad478f0e73
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-07-07 11:35:44 +0900

    Itemモデル編集
    ・バリデーション作成
    ・アクティブハッシュリレーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :user_id, :name, :introduction, :condition, :price, :shipping_charges, :shipping_from, presence: true
+  validates :item_images, presence: true
+  validates :price, :numericality => { :greater_than => 299 }
+  validates :price, :numericality => { :less_than => 9999999 }
+
   has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
-
-  belongs_to :user
   
+  belongs_to :user
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :shipping_from
+  belongs_to_active_hash :shipping_day
 end
