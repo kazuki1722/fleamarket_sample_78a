e755b3433c5f043711795fab210580a8e52a21c0
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-07-07 17:58:45 +0900

    シッピング系アクティブハッシュ作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,8 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :item_images, allow_destroy: true
   
   belongs_to :user
-  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_from
   belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :shipping_day
 end
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,4 +1,4 @@
 class ItemImage < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  mount_uploader :image, ItemImageUploader
   belongs_to :item
 end
