41ec3e4abec01199024d03f50f6c49cd73e13b0b
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-07-08 23:29:07 +0900

    カラム変更記述変更

--- db/migrate/20200702115330_create_items.rb
+++ db/migrate/20200702115330_create_items.rb
@@ -12,29 +12,4 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.timestamps
     end
   end
-  def up
-    change_column :items do |t|
-      t.integer :condition_id, null: false
-      t.integer :shipping_charge_id, null: false
-      t.integer :shipping_day_id, null: false
-      t.integer :prefecture_id, null: false
-    end
-  end
-  def down
-    change_column :items do |t|
-      t.string :condition, null: false
-      t.string :shipping_charges, null: false
-      t.string :shipping_from, null: false
-      t.string :shipping_days, null: false
-    end
-  end
-  def change
-    add_column :items do |t|
-      t.integer :seller_id, null: false
-      t.integer :buyer_id
-      t.references :category, null: false
-      t.string :brand
-    end
-  end
-
 end
