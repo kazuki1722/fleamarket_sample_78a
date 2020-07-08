e3bc1c40f12aa3b3efb5774321322cbaafeebea5
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-07-09 00:04:09 +0900

    Update 20200708142706_change_column_to_item.rb

--- db/migrate/20200708142706_change_column_to_item.rb
+++ db/migrate/20200708142706_change_column_to_item.rb
@@ -1,20 +1,16 @@
 class ChangeColumnToItem < ActiveRecord::Migration[5.2]
-  def up
-    change_column :items, :condition_id, :integer, null: false
-    change_column :items, :shipping_charge_id, :integer, null: false
-    change_column :items, :shipping_day_id, :integer, null: false
-    change_column :items, :prefecture_id, :integer, null: false
-  end
-  def down
-    change_column :items, :condition, :string, null: false
-    change_column :items, :shipping_charges, :string, null: false
-    change_column :items, :shipping_from, :string, null: false
-    change_column :items, :shipping_days, :string, null: false
-  end
   def change
-    add_column :items, :seller_id, :integer, null: false
-    add_column :items, :buyer_id, :integer 
+    remove_column :items, :condition, :string, null: false
+    remove_column :items, :shipping_charges, :string, null: false
+    remove_column :items, :shipping_from, :string, null: false
+    remove_column :items, :shipping_days, :string, null: false
     add_column :items, :category, :references, null: false
     add_column :items, :brand, :string
+    add_column :items, :condition_id, :integer, null: false
+    add_column :items, :prefecture_id, :integer, null: false  
+    add_column :items, :shipping_charge_id, :integer, null: false
+    add_column :items, :shipping_day_id, :integer, null: false
+    add_column :items, :seller_id, :integer, null: false
+    add_column :items, :buyer_id, :integer 
   end
 end
