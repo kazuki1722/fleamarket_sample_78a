d3f848f81c524caebf205ec2639cfe941d7d11bf
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-07-08 23:23:22 +0900

    Update 20200702115330_create_items.rb

--- db/migrate/20200702115330_create_items.rb
+++ db/migrate/20200702115330_create_items.rb
@@ -26,6 +26,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :shipping_charges, null: false
       t.string :shipping_from, null: false
       t.string :shipping_days, null: false
+    end
   end
   def change
     add_column :items do |t|
@@ -33,7 +34,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.integer :buyer_id
       t.references :category, null: false
       t.string :brand
-      
+    end
   end
 
 end
