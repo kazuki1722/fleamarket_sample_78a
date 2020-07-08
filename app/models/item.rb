81c9318e9d769a046500ff3979f495a9940cee03
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-07-09 00:51:33 +0900

    追加カラムに合わせた名前変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :name, length: { maximum: 40 }, presence: true
-  validates :introduction, :condition_id, :price, :shipping_charges_id, :shipping_from_id, :shipping_day_id, presence: true
-  validates :image, presence: true
+  validates :introduction, :condition_id, :price, :shipping_charge_id, :prefecture_id, :shipping_day_id, presence: true
   validates :price, :numericality => { :greater_than => 299 }
   validates :price, :numericality => { :less_than => 9999999 }
 
@@ -13,8 +12,8 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id', optional: true
-  belongs_to_active_hash :shipping_from
+  belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
-  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :shipping_day
 end
