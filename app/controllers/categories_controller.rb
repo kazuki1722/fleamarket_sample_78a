5e5f21fb27873727b657321897d8be4e2437f44f
yuk-hrt <62339023+yuk-hrt@users.noreply.github.com>
2020-07-21 15:01:50 +0900

    カテゴリーページの作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,24 @@
 class CategoriesController < ApplicationController
 
   def index
-    @category_parent_array = Category.where(ancestry: nil)
   end
 
   def show
+    @items = Item.includes(:item_images).order('created_at DESC').limit(4)
+    # @items = Item.including.where(category_id: @category.subtree_ids)
+    # @items = @category..order("created_at DESC").all
+    # カテゴリーidを元に,クリックされたカテゴリーのレコードを取得
+    @category = Category.find(params[:id])
+    # ↓詳細カテゴリページのリンク用
+    # 一階層下のカテゴリーがある場合はそれを格納
+    if @category.has_children?
+      @category_links = @category.children
+    # 無い場合は同じ階層のカテゴリーを格納
+    else
+      @category_links = @category.siblings
+     end
   end
+
+  
 
 end
