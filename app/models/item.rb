725a5bf50f87bcd026fd7b13a0576e2e9e686e4a
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-07-21 15:47:39 +0900

    修正
    ・READE.ME加筆修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,16 +4,14 @@ class Item < ApplicationRecord
   validates :introduction, :condition_id, :category, :price, :shipping_charge_id, :prefecture_id, :shipping_day_id, presence: true
   validates :price, numericality: {greater_than: 299, less_than: 9999999 }
   validates :item_images, presence: true
-  has_many :item_images, dependent: :destroy
+
   has_many :likes, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
-
   has_many :messages, dependent: :destroy
-
+  has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images, allow_destroy: true
   
   belongs_to :user, optional: true
-
   belongs_to :category
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id', optional: true
